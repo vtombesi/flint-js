47b3d88a7342bfa3b58a18301d7f9c9748327bbc
Valentino Tombesi <valentino.tombesi@assist.it>
2017-04-07 17:54:50 +0200

    formatting, fixing

--- src/common/easing/Elastic.js
+++ src/common/easing/Elastic.js
@@ -1,88 +1,68 @@
-export default class Elastic
-{
-  static easeIn(t, b, c, d, a = 0, p = 0)
-  {
-    if (t == 0)
-    {
+export default class Elastic {
+  static easeIn(t, b, c, d, a = 0, p = 0) {
+    if (t === 0) {
       return b;
     }
-    if ((t /= d) == 1)
-    {
+    if ((t /= d) === 1) {
       return b + c;
     }
-    if (!p)
-    {
+    if (!p) {
       p = d * 0.3;
     }
-    var s;
-    if (!a || a < Math.abs(c))
-    {
+    let s;
+    if (!a || a < Math.abs(c)) {
       a = c;
       s = p * 0.25;
     }
-    else
-    {
-      s = p / (2 * Math.PI) * Math.asin(c / a);
+    else {
+      s = (p / (2 * Math.PI)) * Math.asin(c / a);
     }
 
     return -(a * Math.pow(2, 10 * (--t)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
   }
 
-  static easeOut(t, b, c, d, a = 0, p = 0)
-  {
-    if (t == 0)
-    {
+  static easeOut(t, b, c, d, a = 0, p = 0) {
+    if (t === 0) {
       return b;
     }
-    if ((t /= d) == 1)
-    {
+    if ((t /= d) === 1) {
       return b + c;
     }
-    if (!p)
-    {
+    if (!p) {
       p = d * 0.3;
     }
-    var s;
-    if (!a || a < Math.abs(c))
-    {
+    let s;
+    if (!a || a < Math.abs(c)) {
       a = c;
       s = p * 0.25;
     }
-    else
-    {
+    else {
       s = p / (2 * Math.PI) * Math.asin(c / a);
     }
 
     return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
   }
 
-  static easeInOut(t, b, c, d, a = 0, p = 0)
-  {
-    if (t == 0)
-    {
+  static easeInOut(t, b, c, d, a = 0, p = 0) {
+    if (t === 0) {
       return b;
     }
-    if ((t /= d * 0.5) == 2)
-    {
+    if ((t /= d * 0.5) === 2) {
       return b + c;
     }
-    if (!p)
-    {
+    if (!p) {
       p = d * (0.3 * 1.5);
     }
-    var s;
-    if (!a || a < Math.abs(c))
-    {
+    let s;
+    if (!a || a < Math.abs(c)) {
       a = c;
       s = p * 0.25;
     }
-    else
-    {
+    else {
       s = p / (2 * Math.PI) * Math.asin(c / a);
     }
 
-    if (t < 1)
-    {
+    if (t < 1) {
       return -0.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
     }
 
